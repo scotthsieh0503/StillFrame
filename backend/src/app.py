34937238020f9b53265d19e7319f691e22ff671f
Scott Hsieh <hsieh.scott@gmail.com>
2025-03-03 23:27:35 -0800

    create app.py

---
+++ backend/src/app.py
@@ -0,0 +1,27 @@
+import os
+
+from flask import Flask, jsonify
+from flask_cors import CORS
+from .photos import photo_bp
+from .config import Config
+
+ # create and configure the app
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+
+    # Enable CORS
+    CORS(app)
+
+    # load the instance config, if it exists, when not testing
+    app.config.from_object(Config)
+
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+    
+    # Import the blueprint and register it
+    app.register_blueprint(photo_bp, url_prefix='/api/photo')
+    return app
