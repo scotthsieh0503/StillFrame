a5ebb76bc95a7d617b6c516ead848f8cb9777072
Scott Hsieh <hsieh.scott@gmail.com>
2025-03-09 16:45:26 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -23,11 +23,11 @@ flask --app ./src/main.py run --host 0.0.0.0 --port 5000 &
 cd ..
 
 # Start frontend in production mode
-if is_running "npm run serve"; then
+if is_running "npm run serve -- -l 80"; then
     echo "Next.js app is already running. Killing the process..."
-    kill_process "npm run serve"
+    kill_process "npm run serve -- -l 80"
 fi
 
 cd frontend
-npm run serve -- --port 80 &
+npm run serve -- -l 80 &
 cd ..
