7f0556787e204d2a1714101158143382839a7473
Scott Hsieh <hsieh.scott@gmail.com>
2025-03-09 16:08:22 -0700

    update crontab to start when system restarts

--- install.sh
+++ install.sh
@@ -18,4 +18,4 @@ npm install --omit=dev --silent
 echo "Complete"
 
 # Add start.sh to run at startup using crontab
-(crontab -l 2>/dev/null | grep -q "@reboot /Users/scotthsieh/Projects/StillFrame/start.sh") || (crontab -l 2>/dev/null; echo "@reboot /Users/scotthsieh/Projects/StillFrame/start.sh") | crontab -
+(crontab -l 2>/dev/null | grep -q "@reboot /app/StillFrame/start.sh") || (crontab -l 2>/dev/null; echo "@reboot /app/StillFrame/start.sh") | crontab -
