544f0ac80cc7b0d753962be42df5abd784c1fa38
Scott Hsieh <hsieh.scott@gmail.com>
2025-03-09 16:41:57 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,21 +5,29 @@ is_running() {
     pgrep -f "$1" > /dev/null 2>&1
 }
 
-# Start backend in production mode if not already running
-if ! is_running "flask --app ./src/main.py run"; then
-    cd backend
-    source .venv/bin/activate
-    export FLASK_ENV=production
-    flask --app ./src/main.py run --host 0.0.0.0 --port 5000 &
-    cd ..
-else
-    echo "Flask app is already running."
+# Function to kill a running process
+kill_process() {
+    pkill -f "$1"
+}
+
+# Start backend in production mode
+if is_running "flask --app ./src/main.py run"; then
+    echo "Flask app is already running. Killing the process..."
+    kill_process "flask --app ./src/main.py run"
 fi
 
-# Start frontend in production mode if not already running
-if ! is_running "npm run serve"; then
-    cd frontend
-    npm run serve
-else
-    echo "Next.js app is already running."
+cd backend
+source .venv/bin/activate
+export FLASK_ENV=production
+flask --app ./src/main.py run --host 0.0.0.0 --port 5000 &
+cd ..
+
+# Start frontend in production mode
+if is_running "npm run serve"; then
+    echo "Next.js app is already running. Killing the process..."
+    kill_process "npm run serve"
 fi
+
+cd frontend
+npm run serve -- --port 80 &
+cd ..
